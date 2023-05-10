855f82ec1addf22a64762a74d3f902e3e88ddef0
nirkoren1 <nirkoren1@gmail.com>
2023-05-10 20:50:40 +0300

    .

--- untitled/ex22.c
+++ untitled/ex22.c
@@ -347,7 +347,8 @@ int main(int argc, char const *argv[]) {
         if (!c_file_exits) {
             // no c file, write to results.csv
 
-            if(write_grade_to_csv(results_fd, student_entry->d_name, 0) == -1){
+            if(write_grade_to_csv(results_fd, student_entry->d_name, 0) == -1) {
+                printf("Error in: write_grade_to_csv\n");
                 continue;
             }
             continue;
